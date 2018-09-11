684bc02fbfbae79b121643accf2af30ac9a116f0
SID <sidversion9@gmail.com>
2018-09-11 19:26:21 +0900

    secons test

--- screens/Login/index.js
+++ screens/Login/index.js
@@ -1,21 +1,137 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Alert, Button, TextInput, View, StyleSheet, ImageBackground, Image, Dimensions } from 'react-native';
 
-class Login extends Component{
-  render(){
+const bg = require("../../assets/images/bg.png");
+const logo = require("../../assets/images/logo.png");
+const deviceHeight = Dimensions.get("window").height;
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      userId: '',
+      userPass: ''
+    };
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value });
+    for (const key in event) {
+      console.log(key + " : " + event[key])
+    }
+  }
+
+  onLogin() {
+    const { userId, userPass } = this.state;
+
+        fetch('http://13.124.127.253/api/login.php', {
+            method: 'POST',
+            headers: {
+              'Accept' : 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              id: userId,
+              pw: userPass
+            })
+        }).then((response) => response.json())
+          .then((responseJson)=> {
+            if(responseJson === 'data matched') {
+              this.props.navigation.navigate("TabNavi");
+            } else {
+              //alert(responseJson);
+              Alert.alert(
+                '현장통',
+                responseJson
+              )
+            }
+          }).catch((error) => {
+            console.log(error)
+          });
+
+        if (this.props.valid) {
+          this.props.navigation.navigate("TabNavi");
+          return this.props.navigation.dispatch(
+            NavigationActions.reset({
+              index: 0,
+              actions: [NavigationActions.navigate({routeName: "TabNavi"})]
+            })
+          );
+        } else {
+          Alert.alert("아이디나 패스워드를 입력해주세요");
+        }
+  }
+
+  render() {
     return (
-      <View style={style.container}>
-        <Text>Login</Text>
+      <ImageBackground source={bg} style={styles.background}>
+      <View style={styles.container}>
+        <Image source={logo} style={styles.logo} />
+        <TextInput
+          value={this.state.username}
+          onChangeText={(username) => this.setState({ username })}
+          placeholder={'Username'}
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder={'Password'}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        <Button
+          title={'Login'}
+          style={styles.loginBtn}
+          onPress={this.onLogin.bind(this)}
+        />
       </View>
+      </ImageBackground>
     );
   }
 }
-export default Login;
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  }
-})
+  },
+  input: {
+    top: -40,
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    color: "#fff",
+    borderColor: 'black',
+    marginBottom: 10,
+    flexDirection: "row",
+    backgroundColor: "#A4A4A470",
+    borderWidth: 0,
+    borderColor: "transparent",
+    borderRadius: 0,
+  },
+  loginBtn: {
+    top: -100,
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  },
+  background: {
+  flex: 1,
+  width: '100%',
+  height: '100%',
+  backgroundColor: "rgba(0,0,0,0.1)"
+  },
+  logo: {
+  flex: 1,
+  resizeMode: "contain",
+  height:deviceHeight/4,
+  alignSelf: "center"
+  },
+});

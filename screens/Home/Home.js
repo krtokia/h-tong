be1cc6f8f917f1640add8970ba50a22b1d935a6d
SID <sidversion9@gmail.com>
2018-09-16 23:09:01 +0900

    home

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ImageBackground, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { AppRegistry, ListView, ImageBackground, TouchableOpacity, TouchableHighlight, ActivityIndicator } from 'react-native';
 import {
   Container,
   Content,
@@ -14,7 +14,59 @@ import styles from "./styles";
 
 
 class Home extends Component{
+  constructor(props) {
+    super(props);
+    this.state= {
+      isLoading: true,
+
+    }
+  }
+
+  componentDidMount() {
+    return fetch("http://13.124.127.253/api/results.php?page=home")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          isLoading:false,
+          dataSource: ds.cloneWithRows(responseJson),
+        }, function() {
+
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+
+  ListViewItemSeparator = () => {
+    return (
+      <View
+        style= {{
+          height: .5,
+          width: "100%",
+        }}
+      />
+    );
+  }
+
+  fetchData = async () => {
+    const response = await fetch("http://13.124.127.253/api/results.php?page=home");
+    const json = await response.json();
+
+    this.setState({data: json.results});
+    console.log("this is data");
+    console.log(data);
+  };
+
   render(){
+    if (this.state.isLoading) {
+      return (
+        <View Style={{flex:1, paddingTop:20}}>
+          <ActivityIndicator />
+        </View>
+      )
+    }
     return (
       <Container>
         <Content
@@ -37,50 +89,6 @@ class Home extends Component{
                     <Row size={3} style={{flexDirection:"column"}}>
                       <Text style={styles.itemTitle} onPress={() => this.props.navigation.navigate("HomeDetail")}> title </Text>
                       <Text style={styles.itemSub} onPress={() => this.props.navigation.navigate("HomeDetail")}>content</Text>
-                    </Row>
-                  </Col>
-                  <Col style={styles.HomeItems}>
-                    <Row size={7}>
-                      <ImageBackground
-                        source={require("../../assets/images/testImages/2.jpg")}
-                        style={styles.itemImage}
-                        >
-                        <Icon name="more" style={styles.itemIcon}/>
-                      </ImageBackground>
-                    </Row>
-                    <Row size={3} style={{flexDirection:"column"}}>
-                      <Text style={styles.itemTitle}> title </Text>
-                      <Text style={styles.itemSub}>content</Text>
-                    </Row>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col style={styles.HomeItems}>
-                    <Row size={7}>
-                      <ImageBackground
-                        source={require("../../assets/images/testImages/1.jpg")}
-                        style={styles.itemImage}
-                        >
-                        <Icon name="more" style={styles.itemIcon}/>
-                      </ImageBackground>
-                    </Row>
-                    <Row size={3} style={{flexDirection:"column"}}>
-                      <Text style={styles.hItemTitle}> title </Text>
-                      <Text style={styles.hItemSub}>content</Text>
-                    </Row>
-                  </Col>
-                  <Col style={styles.HomeItems}>
-                    <Row size={7}>
-                      <ImageBackground
-                        source={require("../../assets/images/testImages/2.jpg")}
-                        style={styles.itemImage}
-                        >
-                        <Icon name="more" style={styles.itemIcon}/>
-                      </ImageBackground>
-                    </Row>
-                    <Row size={3} style={{flexDirection:"column"}}>
-                      <Text style={styles.hItemTitle}> title </Text>
-                      <Text style={styles.hItemSub}>content</Text>
                     </Row>
                   </Col>
                 </Row>

1dedf5786be3c92b4f5b81b5bbb1341cf7ffc4e9
krtokia <krtokia@gmail.com>
2018-11-02 18:46:25 +0900

    통 정보 수정

--- screens/Home/TongInfo.js
+++ screens/Home/TongInfo.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import {
+  Alert,
   StyleSheet,
   Image,
   TouchableOpacity,
+  Modal,
+  TouchableHighlight,
+  TextInput,
+  ImageBackground
  } from 'react-native';
  import {
    Container,
@@ -16,7 +21,8 @@ import {
    Left,
    Right,
    Body,
-   Footer
+   Footer,
+   Form,
  } from "native-base";
 import MapView from 'react-native-maps';
 import styles from './styles.js';
@@ -31,15 +37,83 @@ class TongInfo extends Component{
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       },
+      modal: false
     };
 
   _handleMapRegionChange = mapRegion => {
     this.setState({ mapRegion });
   };
 
+  modalComplate() {
+    Alert.alert('현장통 생성 기능')
+    this.setState({modal: false});
+  }
+
   render() {
+    var modalFontSize = 11;
     return (
       <Container>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modal}
+          onRequestClose={() => {
+            this.setState({modal: false});
+          }}>
+            <Header style={{height:50,backgroundColor:'#db3928',borderBottomWidth:1,borderBottomColor:'#ccc'}}>
+              <Left style={{flex:1}}>
+                <TouchableHighlight
+                  onPress={() => {
+                    this.setState({modal: false});
+                  }}>
+                  <Icon name='close' type="FontAwesome" style={{color:'#fff'}} />
+                </TouchableHighlight>
+              </Left>
+              <Body style={{flex:4,alignItems:'center'}}>
+                <Text style={{textAlign:'center',color:'#fff',fontSize:18}}>현장통 수정</Text>
+              </Body>
+              <Right  style={{flex:1}}>
+                <Text style={{fontSize:13,color:'#fff'}} onPress={() => {this.modalComplate(!this.state.modal)}}>완료</Text>
+              </Right>
+            </Header>
+            <Content style={{padding:10}}>
+              <Form>
+                <ImageBackground source={require('../../assets/images/backgroundLogo.png')} style={{width:'100%'}}>
+                  <View style={[styles.Row]}>
+                    <View style={styles.createModalStyleLeft}>
+                      <Text style={{fontSize:modalFontSize}}>공사명</Text>
+                      <Text style={{fontSize:modalFontSize}}>건축허가번호</Text>
+                      <Text style={{fontSize:modalFontSize}}>공사 시공자</Text>
+                      <Text style={{fontSize:modalFontSize}}>공사 감리자</Text>
+                      <Text style={{fontSize:modalFontSize}}>발주자</Text>
+                      <Text style={{fontSize:modalFontSize}}>현장 연락처</Text>
+                      <Text style={{fontSize:modalFontSize}}>공가기간</Text>
+                      <Text style={{fontSize:modalFontSize}}>공사규모</Text>
+                      <Text style={{fontSize:modalFontSize}}>현장주소</Text>
+                    </View>
+                    <View style={[styles.createModalStyleRight]}>
+                      <TextInput placeholder="공사명을 입력하세요" underlineColorAndroid='#0000' style={styles.modalInput} />
+                      <TextInput placeholder="건축허가번호를 입력하세요" underlineColorAndroid='#0000' style={styles.modalInput} />
+                      <TextInput placeholder="공사 시공자를 입력하세요" underlineColorAndroid='#0000' style={styles.modalInput} />
+                      <TextInput placeholder="공사 감리자를 입력하세요" underlineColorAndroid='#0000' style={styles.modalInput} />
+                      <TextInput placeholder="발주자를 입력하세요" underlineColorAndroid='#0000' style={styles.modalInput} />
+                      <TextInput placeholder="현장 연락처를 입력하세요" underlineColorAndroid='#0000' style={styles.modalInput} />
+                      <TextInput placeholder="공가기간을 입력하세요" underlineColorAndroid='#0000' style={styles.modalInput} />
+                      <TextInput placeholder="공사규모를 입력하세요" underlineColorAndroid='#0000' style={styles.modalInput} />
+                      <TextInput placeholder="현장주소를 입력하세요" underlineColorAndroid='#0000' style={styles.modalInput} />
+                    </View>
+                  </View>
+                </ImageBackground>
+              </Form>
+              <View style={{width:150,alignSelf:'center',padding:10,marginTop:30}}>
+                <Button transparent rounded block style={{backgroundColor:'#db3928'}}
+                  onPress={() => {this.modalComplate()}}
+                >
+                  <Text style={{color:'#fff'}}>완료</Text>
+                </Button>
+              </View>
+            </Content>
+        </Modal>
         <Header style={{height:70,paddingTop:20,backgroundColor:'#db3928',borderBottomWidth:1,borderBottomColor:'#ccc'}}>
           <Left style={{flex:1}}>
             <Button rounded transparent onPress={() => {this.props.navigation.goBack()}}>
@@ -75,6 +149,7 @@ class TongInfo extends Component{
                       small
                       bordered
                       style={{borderColor:'#db3928',borderWidth:2}}
+                      onPress={() => {this.setState({modal:!this.state.modal})}}
                     >
                       <Text style={{color:'#db3928',fontWeight:'bold'}}>
                         정보수정

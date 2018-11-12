d77d084524311722edb9b3acd99a503508976b07
SID <sidversion9@gmail.com>
2018-11-12 22:23:43 +0900

    chat

--- screens/Chat/ChatRoom.js
+++ screens/Chat/ChatRoom.js
@@ -20,8 +20,33 @@ import {
 } from 'native-base';
 
 import styles from './styles.js';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 class ChatRoom extends Component{
+  state = {
+    messages: [],
+  }
+  componmentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id:1,
+          text: "test",
+          createdAt: new Date(),
+          user: {
+            _id:2,
+            name: 'test2',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    })
+  }
+  onSend(messages=[]) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
   static navigationOptions = ({
     header: null
   });
@@ -39,68 +64,16 @@ class ChatRoom extends Component{
             </Button>
           </Right>
         </Header>
-        <Content
-          showsVerticalScrollIndicator={false}
-          style={{ backgroundColor: "#fff",flexDirection:'column-reverse',flex:1}}
-        >
-            <GetDisplay name="홍길동" info="1" time="오후 5:18"
-              content={`식사는 하셨나요?`} />
-            <SendDisplay info="1" time="오후 5:18"
-              content={`식사는 하셨나요?`} />
-        </Content>
-        <Footer>
-          <View style={{flex:1,backgroundColor:'#f9f9f9',flexDirection:'row'}}>
-            <View style={{flex:1.2,justifyContent:'center',alignItems:'center'}}>
-              <TouchableOpacity>
-                <Icon name="plus-circle" type="FontAwesome" style={{fontSize:40,color:'#555',marginLeft:5}} />
-              </TouchableOpacity>
-            </View>
-            <View style={{flex:9,justifyContent:'center',alignItems:'center'}}>
-              <Item rounded style={{alignSelf:'center',width:'90%',height:40,backgroundColor:'rgba(0,0,0,0.1)',justifyContent:'center'}}>
-                <Input placeholder='채팅 입력' style={{paddingLeft:30,fontSize:13}} />
-                <Icon name="ios-arrow-dropup-circle" style={{color:'#db3928',fontSize:30}} />
-              </Item>
-            </View>
-          </View>
-        </Footer>
+        <GiftedChat
+          placeholder='이곳에 입력해 주세요'
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
       </Container>
     );
   }
 }
 export default ChatRoom;
-
-class GetDisplay extends Component{
-  render() {
-    return(
-        <View style={[styles.chatBase,{alignItems:'flex-start'}]}>
-          <Image source={require('../../assets/images/profile_no.png')} style={[styles.chatThumbnail,{marginLeft:10}]} />
-          <View style={{marginLeft:10,height:'100%',maxWidth:'65%'}}>
-            <Text style={[styles.chatName,{fontSize:13}]}>{this.props.name}</Text>
-            <View style={styles.chatBox}>
-              <Text style={styles.chatContent}>{this.props.content}</Text>
-            </View>
-          </View>
-          <View style={{marginLeft:10,width:'15%',justifyContent:'flex-end',height:'100%'}}>
-            <Text style={{fontSize:10,color:'#db3928'}}>{this.props.info}</Text>
-            <Text style={{fontSize:10,color:'#aaa'}}>{this.props.time}</Text>
-          </View>
-        </View>
-    )
-  }
-}
-
-class SendDisplay extends Component{
-  render() {
-    return(
-        <View style={[styles.chatBase,{flexDirection:'row-reverse'}]}>
-          <View style={[styles.chatBox,{marginRight:10}]}>
-            <Text style={styles.chatContent}>{this.props.content}</Text>
-          </View>
-          <View style={{marginRight:10,width:'15%',justifyContent:'flex-end',height:'100%'}}>
-            <Text style={{fontSize:10,color:'#db3928',textAlign:'right'}}>{this.props.info}</Text>
-            <Text style={{fontSize:10,color:'#aaa',textAlign:'right'}}>{this.props.time}</Text>
-          </View>
-        </View>
-    )
-  }
-}
--- screens/More/Signature.js
+++ screens/More/Signature.js
@@ -21,77 +21,123 @@ import Icon  from 'react-native-vector-icons/FontAwesome';
 import { ImagePicker } from 'expo';
 import styles from "./styles";
 
-var SignaturePad = require('react-native-signature-pad');
+
+import RNDraw from 'rn-draw'
+import pickableImage from "../common.js"
+
 
 class Signature extends Component{
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        id:'sid',
+        type:'signature',
+        imageSource:null
+      }
+      this.uploadSignature = this.uploadSignature.bind(this);
+  //.    this.uploadImage = this.uploadImage.bind(this);
+  }
+
   static navigationOptions = ({
     header: null
   });
 
-  render(){
-    const onChange = async ({ width, height}) => {
-      for (let line of this.sketch.lines) {
-          console.log(line.points)
-        };
-      const options = {
-        format: 'png',
-        quality: 0.1,
-        result:'file',
-        height,
-        width
-      };
-    }
-    _signaturePadError = (error) => {
-      console.error(error);
-    };
+  onDraw() {
+    console.log("draw");
+  }
+  uploadSignature() {
+    const {id, type, imageSource} = this.state;
+    let apiUrl = 'http://13.124.127.253/api/uploadImage.php';
+    let options = null;
 
-    _signaturePadChange = ({base64DataUrl}) => {
-      console.log("Got new signature: " + base64DataUrl);
-    };
-    return (
-      <Container>
+    if (imageSource) {
+      console.log("upload:" + imageSource);
+
+      const formData = new FormData();
+      formData.append('id', id);
+      formData.append('type', type);
+      formData.append('photo', {
+        uri: imageSource,
+        name: `testone`,
+        type: `image/png;base64`,
+      });
+      options = {
+        method: 'POST',
+        body: formData,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'multipart/form-data',
+        },
+      };
+    } else {
+    }
+
+    return fetch(apiUrl, options).then((response) => response.json())
+      .then((responseJson)=> {
+        if(responseJson === 'data matched') {
+          console.log(responseJson);
+          //this.props.navigation.navigate("Main");
+        } else {
+          //alert(responseJson);
+          Alert.alert(
+            '현장통',
+            responseJson
+          )
+        }
+      }).catch((error) => {
+        console.log("EE: " + error)
+      });
+
+  }
+
+    render(){
+
+      return (
+        <Container>
         <Header style={{backgroundColor:'#db3928',justifyContent:'space-between'}}>
-          <Left style={{flex:1}} />
-          <Body style={{justifyContent:'center',alignItems:'center',alignSelf:'flex-end',paddingBottom:10}}>
-            <Text style={{fontSize:20,color:'#fff'}}>전자 서명</Text>
-          </Body>
-          <Right style={{alignSelf:'flex-end',flex:1}}>
-            <Button transparent rounded onPress={() => {this.props.navigation.navigate('Main')}}>
-              <NBIcon name="close" style={{color:'#fff'}} />
-            </Button>
-          </Right>
+        <Left style={{flex:1}} />
+        <Body style={{justifyContent:'center',alignItems:'center',alignSelf:'flex-end',paddingBottom:10}}>
+        <Text style={{fontSize:20,color:'#fff'}}>전자 서명</Text>
+        </Body>
+        <Right style={{alignSelf:'flex-end',flex:1}}>
+        <Button transparent rounded onPress={() => {this.props.navigation.navigate('Main')}}>
+        <NBIcon name="close" style={{color:'#fff'}} />
+        </Button>
+        </Right>
         </Header>
         <Content
-          style={{ backgroundColor: "#fff" }}
-          contentContainerStyle={{ flex: 1 }}
+        style={{ backgroundColor: "#fff" }}
+        contentContainerStyle={{ flex: 1 }}
         >
-          <View style={[styles.container,{marginTop:10,padding:20}]}>
-            <View style={{flex:5,width:'100%',borderWidth:5,borderColor:'#ccc',borderRadius:10}}>
-            <SignaturePad onError={this._signaturePadError}
-                          onChange={this._signaturePadChange}
-                          style={{flex: 1, backgroundColor: 'white'}}/>
-            </View>
+        <View style={[styles.container,{marginTop:10,padding:20}]}>
+        <View style={{flex:5,width:'100%',borderWidth:5,borderColor:'#ccc',borderRadius:10}}>
+        <RNDraw
+          containerStyle={{backgroundColor: 'rgba(0,0,0,0.01)'}}
+            onChangeStrokes={this.onDraw}
+        />
+        </View>
 
-            <View style={{alignItems:'flex-end',width:'100%',flex:1,justifyContent:'flex-end'}}>
-              <View style={{borderBottomWidth:1,borderBottomColor:'#db3928',flexDirection:'row',alignItems:'flex-end'}}>
-                <NBIcon name="edit" type="FontAwesome" style={{color:'#db3928',fontSize:15}} />
-                <Text style={{color:'#db3928',fontSize:15}} > 다시서명</Text>
-              </View>
-            </View>
-            <View style={{width:'100%',flex:3,justifyContent:'center'}}>
-              <Text style={{fontSize:14}}>{`※ 등록하신 전자서명은 노무비 선지급을 위한 위임장과 노무비 청구 시 사용됩니다.`}</Text>
-            </View>
-            <View style={{width:'60%',flex:1}}>
-              <Button full rounded style={{backgroundColor:'#db3928'}}>
-                <Text style={{fontSize:18}}>저장</Text>
-              </Button>
-            </View>
-            <View style={{flex:2}} />
-          </View>
+        <View style={{alignItems:'flex-end',width:'100%',flex:1,justifyContent:'flex-end'}}>
+        <View style={{borderBottomWidth:1,borderBottomColor:'#db3928',flexDirection:'row',alignItems:'flex-end'}}>
+        <NBIcon name="edit" type="FontAwesome" style={{color:'#db3928',fontSize:15}} />
+        <TouchableOpacity  onPress={this.onClear} ><Text style={{color:'#db3928',fontSize:15}}> 다시서명</Text></TouchableOpacity>
+        </View>
+        </View>
+        <View style={{width:'100%',flex:3,justifyContent:'center'}}>
+        <Text style={{fontSize:14}}>{`※ 등록하신 전자서명은 노무비 선지급을 위한 위임장과 노무비 청구 시 사용됩니다.`}</Text>
+        </View>
+        <View style={{width:'60%',flex:1}}>
+        <Button full rounded style={{backgroundColor:'#db3928'}} onPress={this.uploadSignature}>
+        <Text style={{fontSize:18}}>저장</Text>
+        </Button>
+        </View>
+        <View style={{flex:2}} />
+        </View>
         </Content>
-      </Container>
-    );
+        </Container>
+      );
+    }
   }
-}
 
-export default Signature;
+  export default Signature;
